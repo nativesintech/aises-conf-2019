9d9e6618f8882f64112d3627492adf9834c37623
Adam Recvlohe <adam.recvlohe@gmail.com>
2019-10-10 10:04:05 -0500

    Update slides and colors 💅

--- presentation/theme/index.js
+++ presentation/theme/index.js
@@ -1,58 +1,58 @@
 import createTheme from "../../createTheme";
 
 const colors = {
-  primary: "white",
-  secondary: "#1F2022", // almost black w/ blueish tint
-  tertiary: "#fc300f", // bright red
-  quaternary: "#CECECE" // pale gray
+  primary: "#FFF",
+  secondary: "#0F2944",
+  tertiary: "#4DD1DC",
+  quaternary: "#F0F4FB"
 };
 
 const theme = createTheme(colors, {
-    primary: "Montserrat",
-    secondary: "Helvetica"
-  }, {
-    progress: {
-      pacmanTop: {
-        background: colors.quaternary
+  primary: "Montserrat",
+  secondary: "Helvetica"
+}, {
+  progress: {
+    pacmanTop: {
+      background: colors.quaternary
+    },
+    pacmanBottom: {
+      background: colors.quaternary
+    },
+    point: {
+      borderColor: colors.quaternary
+    }
+  },
+  components: {
+    heading: {
+      h1: {
+        fontSize: '4rem',
+        textTransform: 'uppercase'
       },
-      pacmanBottom: {
-        background: colors.quaternary
+      h2: {
+        fontSize: '3.5rem',
+        textTransform: 'uppercase'
       },
-      point: {
-        borderColor: colors.quaternary
+      h3: {
+        fontSize: '3rem',
+        textTransform: 'uppercase'
+      },
+      h4: {
+        fontSize: '2.5rem',
+        textTransform: 'uppercase'
+      },
+      h5: {
+        fontSize: '2rem',
+        textTransform: 'uppercase'
+      },
+      h6: {
+        fontSize: '1.5rem',
+        textTransform: 'uppercase'
       }
     },
-    components: {
-      heading: {
-        h1: {
-          fontSize: '4rem',
-          textTransform: 'uppercase'
-        },
-        h2: {
-          fontSize: '3.5rem',
-          textTransform: 'uppercase'
-        },
-        h3: {
-          fontSize: '3rem',
-          textTransform: 'uppercase'
-        },
-        h4: {
-          fontSize: '2.5rem',
-          textTransform: 'uppercase'
-        },
-        h5: {
-          fontSize: '2rem',
-          textTransform: 'uppercase'
-        },
-        h6: {
-          fontSize: '1.5rem',
-          textTransform: 'uppercase'
-        }
-      },
-      codePane: {
-        fontSize: '2rem'
-      }
+    codePane: {
+      fontSize: '2rem'
     }
-  });
+  }
+});
 
 export default theme;
